8c0e96d22fb04c4914e36ec1e5d7d529e5795cca
dong-geon-Lee <ch9716@naver.com>
2022-04-17 14:31:57 +0900

    make user routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,40 @@
 const User = require("../models/users");
+const asyncHandler = require("express-async-handler");
 
-const getUsers = (req, res) => {
-  res.status(200).json("Hello World! hey!");
-};
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
 
-const postUser = (req, res) => {
-  res.status(200).json("Hello World! hey1!");
-};
+  res.status(200).json(users);
+});
 
-const putUser = (req, res) => {
-  res.status(200).json("Hello World! he2y!");
-};
+const postUser = asyncHandler(async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-const deleteUser = (req, res) => {
-  res.status(200).json("Hello World! he3y!");
-};
+  const newUser = await user.save();
+
+  res.status(200).json(newUser);
+});
+
+const putUser = asyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  !user && res.status(400).json({ message: "Not found user email." });
+
+  const newUser = await User.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(newUser);
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+  await User.findOneAndDelete(req.params.id);
+
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = { getUsers, postUser, putUser, deleteUser };
