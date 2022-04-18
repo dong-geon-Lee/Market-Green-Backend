f78f47334762c6420620b713e56bcdd9150e90ea
dong-geon-Lee <ch9716@naver.com>
2022-04-18 12:26:40 +0900

    add crypoto update logic

--- controllers/users.js
+++ controllers/users.js
@@ -2,12 +2,6 @@ const User = require("../models/users");
 const asyncHandler = require("express-async-handler");
 const CryptoJS = require("crypto-js");
 
-const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find();
-
-  res.status(200).json(users);
-});
-
 const registerUser = asyncHandler(async (req, res) => {
   const user = new User({
     name: req.body.name,
@@ -21,24 +15,6 @@ const registerUser = asyncHandler(async (req, res) => {
   const newUser = await user.save();
 
   res.status(200).json(newUser);
-});
-
-const updateUser = asyncHandler(async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-
-  !user && res.status(400).json({ message: "Not found user email." });
-
-  const newUser = await User.findOneAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-
-  res.status(200).json(newUser);
-});
-
-const deleteUser = asyncHandler(async (req, res) => {
-  await User.findOneAndDelete(req.params.id);
-
-  res.status(200).json({ id: req.params.id });
 });
 
 const loginUser = asyncHandler(async (req, res) => {
@@ -60,11 +36,40 @@ const loginUser = asyncHandler(async (req, res) => {
     return res.status(400).json("Wrong password!");
   }
 
-  // if (user.password !== req.body.password) {
-  //   return res.status(400).json({ message: "Password not correct!" });
-  // }
-
   res.status(200).json(user);
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString();
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+
+  !user && res.status(400).json({ message: "Not found user email." });
+
+  const newUser = await User.findOneAndUpdate(
+    req.params.id,
+    { $set: req.body },
+    { new: true }
+  );
+
+  res.status(200).json(newUser);
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+  await User.findOneAndDelete(req.params.id);
+
+  res.status(200).json({ id: req.params.id });
+});
+
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json(users);
 });
 
 module.exports = { getUsers, registerUser, updateUser, deleteUser, loginUser };
