be16bb2ac7a38385d0f2ff9ecab5b6254bcf9135
dong-geon-Lee <ch9716@naver.com>
2022-06-23 16:47:34 +0900

    product logic change & image uploads

--- routes/products.js
+++ routes/products.js
@@ -16,7 +16,7 @@ const upload = require("../middleware/multerUpload");
 router.get("/", getProducts);
 router.get("/review/:id", getReviews);
 router.delete("/review/:id", deleteReview);
-router.get("/:id", protect, getProduct);
+router.get("/:id", getProduct);
 router.post("/", tokenAdmin, upload.single("img"), setProduct);
 router.post("/:id/review", protect, createReview);
 router.put("/:id", tokenAdmin, upload.single("img"), updateProduct);
