d91086f255f9d7ad65262c054f4c72c237cd7a73
dong-geon-Lee <ch9716@naver.com>
2022-06-29 15:35:24 +0900

    chage

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.get("/api/paypal", (req, res) => {
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  app.use(express.static("build"));
 
-  app.get("/", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
