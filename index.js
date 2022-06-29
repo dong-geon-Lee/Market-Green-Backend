aafe3ccfa7310b02a73703647e963e2c9164b734
dong-geon-Lee <ch9716@naver.com>
2022-06-29 14:50:44 +0900

    change production

--- index.js
+++ index.js
@@ -27,12 +27,11 @@ app.get("/api/paypal", (req, res) => {
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+if (process.env.NODE_ENV) {
+  app.use(express.static(path.resolve(process.cwd(), "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(process.cwd(), "frontend/build/index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
