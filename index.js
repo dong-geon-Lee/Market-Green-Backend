ec37def72c6158ac2ac977ce377f53ba249ddac0
dong-geon-Lee <ch9716@naver.com>
2022-06-29 15:58:30 +0900

    procfilea

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.get("/api/paypal", (req, res) => {
 
 //  -----Deployment-----
 
-const __dirname1 = path.resolve();
+__dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/build")));
 
-  app.get("/", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
